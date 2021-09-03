1320211509fe28c4a386cd6b4a2c17b9797345de
SEBASTIAN RAMIREZ <sebastianerz@gmail.com>
2021-09-02 23:50:30 -0600

    Progress on evaluate method

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -5,6 +5,10 @@ namespace FormulaEvaluator
 {
     public static class Evaluator
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
         public delegate int Lookup(String v);
         Stack vals = new Stack();
         Stack operators = new Stack();
@@ -13,14 +17,21 @@ namespace FormulaEvaluator
             string[] substrings = Regex.Split(s, "(\()|(\))|(-)|(\+)|(\*)|(/)");
             for(int i = 0; i < substrings.Length; i++)
             {
-                if(Char.IsDigit(substrings[i]))
+                if (Char.IsDigit(substrings[i]))
+                {
                     if (operators.Peek.equals("*") || operators.Peek.equals("/"))
                     {
-                        
+                        vals.Push(Evaluator.Calculate(int.Parse(substrings[i]), vals.Pop(), operators.Pop());
                     }
+                    else
+                        vals.Push(int.Parse(substring[i]));
+
+                }
+                
+
             }
         }
-        private int Calculate(int a, int b, String op)
+        private static int Calculate(int a, int b, String op)
         {
             if (op.Equals("+");
                 return a + b;
@@ -31,7 +42,18 @@ namespace FormulaEvaluator
             else 
                 return a / b;
         }
-      
+        private static bool IsVariable(string s)
+        {
+            bool letters = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetter(s[i]) && letters == true)
+                    continue;
+
+            }
+        }
+
+
     }
 
    
