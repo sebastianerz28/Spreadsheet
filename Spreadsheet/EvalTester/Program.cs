86940952e32d089e5a4edb422963ba6de354d969
SEBASTIAN RAMIREZ <sebastianerz@gmail.com>
2021-09-03 14:57:07 -0600

    Tests Written and Sizeable debugging completed

--- Spreadsheet/EvalTester/Program.cs
+++ Spreadsheet/EvalTester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using FormulaEvaluator;
+using static FormulaEvaluator.Evaluator;
 
 namespace EvalTester
 {
@@ -6,7 +8,58 @@ namespace EvalTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Tests for Basic Addition
+            Console.WriteLine("Answer should be 3 and was: " + Evaluator.Evaluate("1+2", l));
+            Console.WriteLine("Answer should be 6 and was: " + Evaluator.Evaluate("1+2+3", l));
+            Console.WriteLine("Answer should be 10 and was: " + Evaluate("1+2+3+4", l));
+            Console.WriteLine("Answer should be 15 and was: " + Evaluate("1+2+3+4+5", l));
+
+            //Tests For subtraction
+            Console.WriteLine("Answer should be 0 and was: " + Evaluator.Evaluate("1-1", l));
+            Console.WriteLine("Answer should be 1 and was: " + Evaluator.Evaluate("2-1", l));
+            Console.WriteLine("Answer should be -1 and was: " + Evaluator.Evaluate("1-2", l));
+            Console.WriteLine("Answer should be -35 and was: " + Evaluator.Evaluate("10-9-8-7-6-5-4-3-2-1", l));
+
+            //Tests For Multiplication
+            Console.WriteLine("Answer Should be 0 and was: " + Evaluate("0*0",l));
+            Console.WriteLine("Answer Should be 2 and was: " + Evaluate("1*2", l));
+            Console.WriteLine("Answer Should be 8 and was: " + Evaluate("2*2*2", l));
+            Console.WriteLine("Answer Should be 10 and was: " + Evaluate("1*2*5", l));
+            //Tests for Division
+            Console.WriteLine("Answer Should be 0 and was: " + Evaluate("1/2", l));
+
+            //Test basic operations plus variable
+            Console.WriteLine("Answer should be 2 and was: " + Evaluate("1+a1" , l));
+
+            //Test Expression
+            Console.WriteLine("Answer Should be 3 and was: " + Evaluate("(1+2)", l));
+            Console.WriteLine("Answer Should be 0 and was: " + Evaluate("(1/2)", l));
+            Console.WriteLine("Answer Should be 2 and was: " + Evaluate("(1*2)", l));
+            Console.WriteLine("Answer Should be -1 and was: " + Evaluate("(1-2)", l));
+
+            Console.WriteLine("Answer Should be 2 and was: " + Evaluate("(1-2) + 3", l));
+            Console.WriteLine("Answer should be 3 and was: " + Evaluate("a1 + (1+1)", l));
+            Console.WriteLine("answer should be 2 and was " + Evaluate("3 + 3 / b3",  l));
+        }
+        public static int l(string s)
+        {
+
+            if(s == "a1")
+            {
+                return 1;
+
+            }
+            else if (s == "b1")
+            {
+                return 2;
+            }
+            else if (s == "b3")
+            {
+                return 3;
+            }
+            else
+                return 0;
         }
     }
 }
--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -18,11 +18,13 @@ namespace FormulaEvaluator
                 if (int.TryParse(substrings[i], out temp))
                 {
                     if (vals.Count == 0)
-                        throw new ArgumentException("No values to compute");
-                    else if (vals.Peek().Equals(0) && int.Parse(substrings[i]) == 0 && operators.Peek() == "/")
+                        vals.Push(int.Parse(substrings[i]));
+                    else if (vals.Peek().Equals(0) && int.Parse(substrings[i]) == 0 && operators.Peek().Equals("/"))
                         throw new ArithmeticException("divide by 0");
                     else if (operators.Peek().Equals("*") || operators.Peek().Equals("/"))
                     {
+                        if (vals.Count == 0)
+                            throw new ArgumentException("No values to compute");
                         vals.Push(Evaluator.Calculate(int.Parse(substrings[i]), (int)vals.Pop(), (string)operators.Pop()));
                     }
                     else
@@ -31,45 +33,53 @@ namespace FormulaEvaluator
                 }
                 else if (IsVar(substrings[i]))
                 {
-                    if (vals.Peek() == null)
-                        throw new ArgumentException("No values to compute");
-                    else if (vals.Peek().Equals(0) && variableEvaluator(substrings[i]) == 0 && operators.Peek() == "/")
+                    substrings[i] = trim(substrings[i]);
+                    if (vals.Count == 0)
+                    {
+                        vals.Push(variableEvaluator(substrings[i]));
+                    }
+                        
+                    else if (vals.Peek().Equals(0) && variableEvaluator(substrings[i]) == 0 && operators.Peek().Equals("/"))
                         throw new ArithmeticException("divide by 0");
                     else if (operators.Peek().Equals("*") || operators.Peek().Equals("/"))
                     {
+                        if (vals.Peek() == null)
+                            throw new ArgumentException("No values to compute");
                         vals.Push(Evaluator.Calculate(variableEvaluator(substrings[i]), (int) vals.Pop(), (string)operators.Pop()));
                     }
                     else
-                        vals.Push(int.Parse(substrings[i]));
+                        vals.Push(variableEvaluator(substrings[i]));
                 }
-                else if (substrings[i] == "+" || substrings[i] == "-")
+                else if (substrings[i].Equals("+") || substrings[i].Equals("-"))
                 {
-
-                    if (operators.Peek() == "+" || operators.Peek() == "-")
+                    if(operators.Count == 0)
+                        operators.Push(substrings[i]);
+                    else if (operators.Peek().Equals("+") || operators.Peek().Equals("-"))
                     {
                         if (vals.Count < 2)
                             throw new ArgumentException("Not enough values");
                         else
                         {
-                            int x = int.Parse((string)vals.Pop());
-                            int y = int.Parse((string)vals.Pop());
+                            int x = (int) vals.Pop();
+                            int y = (int)vals.Pop();
                             vals.Push(Calculate(x, y, substrings[i]));
                         }
                     }
                     else
                         operators.Push(substrings[i]);
+
                 }
-                else if (substrings[i] == "*" || substrings[i] == "/")
+                else if (substrings[i].Equals("*") || substrings[i].Equals("/"))
                 {
                     operators.Push(substrings[i]);
                 }
-                else if (substrings[i] == "(")
+                else if (substrings[i].Equals("("))
                 {
                     operators.Push(substrings[i]);
                 }
-                else if (substrings[i] == ")")
+                else if (substrings[i].Equals(")"))
                 {
-                    if (operators.Peek() == "+" || operators.Peek() == "-")
+                    if (operators.Peek().Equals("+") || operators.Peek().Equals("-"))
                     {
                         if (vals.Count < 2)
                             throw new ArgumentException("Not enough values");
@@ -80,12 +90,14 @@ namespace FormulaEvaluator
                             vals.Push(Calculate(x, y, (string)operators.Pop()));
                         }
                     }
-                    if (operators.Peek() == "(")
+                    if (operators.Peek().Equals("("))
                     {
                         operators.Pop();
                     }
                     else
                         throw new ArgumentException("Expected left parenthesis: ( but was different");
+                    if (operators.Count == 0)
+                        continue;
                     if (operators.Peek().Equals("*") || operators.Peek().Equals("/"))
                     {
                         if (vals.Count < 2)
@@ -99,13 +111,13 @@ namespace FormulaEvaluator
                 }
             }
             if (operators.Count == 0)
-                if (vals.Count > 1)
+                if (vals.Count != 1)
                     throw new ArgumentException("too many values");
                 else
                     return (int)vals.Pop();
             else
             {
-                if (operators.Count == 1 && (operators.Peek() == "+" || operators.Peek() == "-") && vals.Count == 2)
+                if (operators.Count == 1 && (operators.Peek().Equals("+") || operators.Peek().Equals("-")) && vals.Count == 2)
                 {
                     int x = (int)vals.Pop();
                     int y = (int)vals.Pop();
@@ -113,7 +125,7 @@ namespace FormulaEvaluator
                 }
                 else if (operators.Count != 1)
                     throw new ArgumentException("too many operators");
-                else if (operators.Peek() != "+" || operators.Peek() != "-")
+                else if (operators.Peek().Equals("+") || operators.Peek().Equals("-"))
                     throw new ArgumentException("wrong type of operators");
                 else
                     throw new ArgumentException("too many vals");
@@ -125,31 +137,42 @@ namespace FormulaEvaluator
             if (op.Equals("+"))
                 return a + b;
             else if (op.Equals("-"))
-                return a - b;
+                return b - a;
             else if (op.Equals("*"))
                 return a * b;
             else
-                return a / b;
+                return b / a;
         }
         private static bool IsVar(string s)
         {
-            bool isBool = true;
-            bool letters = true;
-            for (int i = 0; i < s.Length; i++)
+            if (s.Length == 0 || s.Equals(" "))
+                return false;
+            else if (s.StartsWith(" "))
+                s = s.Remove(s[0]);
+            else if (s.EndsWith(" "))
+                s = s.Remove(s.Length-1);
+             
+            
+            int i = 0;
+            
+            while (Char.IsLetter(s[i]))
+                i++;
+            while (Char.IsDigit(s[i]))
             {
-                if (Char.IsLetter(s[i]) && letters == true)
-                    continue;
-                else if (Char.IsLetter(s[i]) == false && letters == true)
-                    letters = false;
-                else if (letters == false && Char.IsDigit(s[i]))
-                {
-                    continue;
-                }
-                else
-                    return false;
-
+                i++;
+                if (i == s.Length)
+                    break;
             }
-            return true;
+
+            return i == s.Length;
+        }
+        private static string trim(string s)
+        {
+            if (s.StartsWith(" "))
+                s = s.Remove(s[0]);
+            if (s.EndsWith(" "))
+                s = s.Remove(s.Length - 1);
+            return s;
         }
     }
 
